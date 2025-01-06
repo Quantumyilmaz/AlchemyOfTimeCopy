b9564194391bfa560407adc12d911ab03ce1b8be
Ege Yilmaz <47591838+Quantumyilmaz@users.noreply.github.com>
2025-01-06 15:19:02 +0100

    new option UnOwnedObjectsEvolve

--- src/Settings.cpp
+++ src/Settings.cpp
@@ -821,6 +821,7 @@ void LoadINISettings()
     Settings::disable_warnings = ini.GetBoolValue("Other Settings", "DisableWarnings", Settings::disable_warnings);
     Settings::world_objects_evolve = ini.GetBoolValue("Other Settings", "WorldObjectsEvolve", Settings::world_objects_evolve);
 	Settings::placed_objects_evolve = ini.GetBoolValue("Other Settings", "PlacedObjectsEvolve", Settings::placed_objects_evolve);
+	Settings::unowned_objects_evolve = ini.GetBoolValue("Other Settings", "UnOwnedObjectsEvolve", Settings::unowned_objects_evolve);
 		
     ini.SaveFile(Settings::INI_path);
 }
